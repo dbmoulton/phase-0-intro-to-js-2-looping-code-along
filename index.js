c2a1ffd59a9d5cd339b13e9fc961ce3207e58268
David Moulton <dbmoulton@gmail.com>
2023-02-12 22:56:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = [] 
+    for (let i = 0; i < names.length; i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(n) {
+    let countDown = n;
+    while (countDown >= 0) {
+      console.log(countDown--);
+    }  
+    }
